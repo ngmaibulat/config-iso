a20fc56679d48fc6957af96b6523fcc722601ace
Aibulat <ngmaibulat@gmail.com>
2024-02-10 08:30:34 +0000

    pull repo before build

--- build.sh
+++ build.sh
@@ -3,11 +3,16 @@
 export dir=./arch
 export out=/var/iso
 export run=/var/build
+export repo=$dir/airootfs/root/setup
 
 rm -fr $run
 
 mkdir -p $out
 mkdir -p $run
 
+git clone https://github.com/ngmaibulat/config-dotfiles.git $repo
+
 mkarchiso -v -w $run -o $out $dir
+
+rm -fr $repo
 
